78f714b6079960161fa95cbbde5b896edd3e00ae
Sean West <swest06@dcs.bbk.ac.uk>
2018-02-27 16:59:34 +0000

    First initialization of repo

---
+++ test/fraction/FractionTest.java
@@ -0,0 +1,18 @@
+package fraction;
+
+import org.junit.Test;
+
+public class FractionTest {
+
+    // complete the tests and add more
+
+    @Test
+    public void testAdd() {
+        // Tests that are expected to succeed or fail
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        // test that should throw an ArithmeticException
+    }
+}
