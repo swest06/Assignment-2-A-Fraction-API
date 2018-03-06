482da21043453d36991273523df91bf4c0f5817c
Sean West <swest06@dcs.bbk.ac.uk>
2018-03-06 19:38:00 +0000

    Write testNegate()

--- test/fraction/FractionTest.java
+++ test/fraction/FractionTest.java
@@ -11,6 +11,8 @@ public class FractionTest{
 
 
     // complete the tests and add more
+    // Tests that are expected to succeed or fail
+    
     @Test
     public void testFractionImpl() {
 //        assertEquals(2, frac1.numerator);
@@ -39,7 +41,7 @@ public class FractionTest{
         //Test
         assertEquals(frac1, frac2.add(frac3));
 
-        // Tests that are expected to succeed or fail
+
     }
 
     @Test
@@ -93,6 +95,21 @@ public class FractionTest{
         //Test
         assertEquals(awns, frac1.abs());
         assertEquals(awns, frac2.abs());
+    }
+
+    @Test
+    public void testNegate(){
+        //Expected answers
+        FractionImpl ans1 = new FractionImpl(-1,4);
+        FractionImpl ans2 = new FractionImpl(1,2);
+
+        //Objects/Arguments
+        FractionImpl frac1 = new FractionImpl(1,4);
+        FractionImpl frac2 = new FractionImpl(-1,2);
+
+        //Test
+        assertEquals(ans1, frac1.negate());
+        assertEquals(ans2, frac2.negate());
     }
 
     @Test(expected = ArithmeticException.class)
